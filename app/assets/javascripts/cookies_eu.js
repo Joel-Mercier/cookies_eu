e3954c6fd600dea579a8d8d434a9fc94b8a48165
Joel Mercier <jomercier@sfr.fr>
2017-11-14 09:15:07 +0100

    add turbolinks 5 support

--- app/assets/javascripts/cookies_eu.js
+++ app/assets/javascripts/cookies_eu.js
@@ -39,13 +39,13 @@ var cookiesEu = {
 
   if (document.addEventListener) {
     document.addEventListener('turbolinks:load', isReady, false);
-    document.addEventListener('DOMContentLoaded', isReady, false);
+    //document.addEventListener('DOMContentLoaded', isReady, false);
   }
 
   // Old browsers IE < 9
-  if (window.addEventListener) {
-    window.addEventListener('load', isReady, false);
-  } else if (window.attachEvent) {
-    window.attachEvent('onload', isReady);
-  }
+  // if (window.addEventListener) {
+    // window.addEventListener('load', isReady, false);
+  // } else if (window.attachEvent) {
+    // window.attachEvent('onload', isReady);
+  // }
 })();
